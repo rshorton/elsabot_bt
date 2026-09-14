ecfe8ab8e9d9043084ecebcfb06c42d7ac8b8dbe
Scott Horton <horton.rscott@gmail.com>
2026-09-13 20:31:09 -0500

    Add missing includes

--- src/base64.hpp
+++ src/base64.hpp
@@ -1,4 +1,7 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
 std::string base64_encode(const std::vector<unsigned char>& data);
 std::vector<unsigned char> base64_decode(const std::string& in);
